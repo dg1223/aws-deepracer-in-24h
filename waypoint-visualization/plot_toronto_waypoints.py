9580b4671beb327f847c3f6dcfefca71e6df7319
Shamir Alavi <dg1223@users.noreply.github.com>
2019-10-31 09:48:01 -0400

    plot waypoints

---
+++ waypoint-visualization/plot_toronto_waypoints.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+track_name = "Canada_Training"
+absolute_path = "."
+waypoints = np.load("%s/%s.npy" % (absolute_path, track_name))
+
+print("Number of waypoints = " + str(waypoints.shape[0]))
+for i, point in enumerate(waypoints):
+    waypoint = (point[2], point[3])
+    plt.scatter(waypoint[0], waypoint[1])
+    print("Waypoint " + str(i) + ": " + str(waypoint))
+
+plt.show()
